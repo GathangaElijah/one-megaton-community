ecad8a51d78fcfdf96f2c65c9fa618b4e28170ca
GathangaElijah <elyg3672@gmail.com>
2025-08-28 11:14:41 +0300

    refactor: change the class name

--- one-megaton-community/src/App.jsx
+++ one-megaton-community/src/App.jsx
@@ -10,26 +10,30 @@ function App() {
     
     <>
      <div className="hero-section">
-          <article>
-          <div className='main-heading'>
-            The One Megaton Community
-          </div>
-          <p>
-            Reducing carbon emissions through individual
-            and collective action.
-            Join a community of change-makers committed to
-            reduce carbon emmissions and building a sustainable future.
-            Our community provides the tools, resources and
-            connections you need to amplify your environmental impact.
-          </p>
-            
-          </article>
+        <div className='main-heading'>
+          The One Megaton Community
+        </div>
+        <div className='article'>
+          Reducing carbon emissions through individual
+          and collective action.
+          Join a community of change-makers committed to
+          reduce carbon emmissions and building a sustainable future.
+          Our community provides the tools, resources and
+          connections you need to amplify your environmental impact.
+        </div>
+     
+        <div className='registration-form'>
           <RegistrationForm />
+        </div>
+
       </div>
+
       <footer>
         <CommunityImages />
         <img src={footerImage} alt="Chicago cityscape" />
       </footer>
+
+
     </>
   )
 }
